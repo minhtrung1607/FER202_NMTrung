cdcd12aa6d6053abc0510d2bf26553abe67cd712
minhtrung1607 <bondz1607@gmail.com>
2026-05-14 13:16:02 +0700

    add slot 2 ex1

---
+++ slot2/ex1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>MinhTrung</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/nguyenminhtrung.167/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Facebook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
