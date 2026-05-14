ae3261642e7cd69a76f327f332630123185a5443
minhtrung1607 <bondz1607@gmail.com>
2026-05-14 13:16:47 +0700

    add slot 2 ex2

---
+++ slot2/ex2/src/demo.js
@@ -0,0 +1,14 @@
+let tong = (a, b) => a + b;
+//goi ham tong voi 2 so 5, sau đó in kết quả ra console
+
+console.log(tong(2, 5));
+let chao = () => console.log("Xin chào mọi người!");
+//goi ham chao de in ra chu "Xin chào mọi người!"
+chao();
+// viet ham chao 2 de in ra chu "Xin chào, tôi là một hàm arrow function!"
+let chao2 = () => console.log("Xin chào, tôi là minhtrung!");
+chao2();
+// viet ham chao2 nhan 1 tham so la 1 doi tuong Person co cac thuoc tinh name ,id,age,address, sau do in ra chu "Xin chào, tôi là [name], tôi [age] tuổi, sống tại [address]"
+let chao3 = (person) => console.log(`Xin chào, tôi là ${person.name}, tôi ${person.age} tuổi, sống tại ${person.address}`);
+let person = { name: "minhtrung", id: 1, age: 21, address: "da nang" };
+chao3(person);
